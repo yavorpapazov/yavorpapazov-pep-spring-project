2b3b791d046d1dc2a93806ab233089ae66cbebe5
Yavor Papazov <yavorchap@gmail.com>
2023-10-28 04:25:10 +0000

    Completed 3 tests

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,11 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import com.example.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -8,5 +14,11 @@ package com.example.controller;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
-
+    @Autowired
+	AccountService accountService;
+            
+    @PostMapping("/register")
+    public Account addAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,18 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+
 public class AccountService {
+    AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account createAccount(Account account){
+        return accountRepository.save(account);
+    }
 }
