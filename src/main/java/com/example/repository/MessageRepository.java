3673e3fb01710f76c7bb667adef9ef652503f13b
Yavor Papazov <yavorchap@gmail.com>
2023-10-29 19:58:49 +0000

    Completed get all messages for user

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -5,9 +5,12 @@ import com.example.entity.Message;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
-    List<Message> getMessagesByPostedBy(Integer posted_by);
+    @Query("FROM Message WHERE posted_by = :userId")
+    List<Message> getMessagesByPostedBy(@Param("userId") Integer posted_by);
 }
